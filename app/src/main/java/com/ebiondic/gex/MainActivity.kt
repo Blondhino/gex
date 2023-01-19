4bd099b80b240e360a1d79f6fad9ea32bb4f7f06
Enio Biondić <37383564+Blondhino@users.noreply.github.com>
2023-01-19 20:04:45 +0100

    update icon add splash screen for sdk lvl < 31

--- app/src/main/java/com/ebiondic/gex/MainActivity.kt
+++ app/src/main/java/com/ebiondic/gex/MainActivity.kt
@@ -4,10 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material.Text
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
+    installSplashScreen()
     setContent {
       Text("We have compose running !!")
     }
